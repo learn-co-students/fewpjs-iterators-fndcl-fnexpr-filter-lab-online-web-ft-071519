daa71f52d309ec7697e5f332aea9b13d0a43dcac
Alethia Quintero <alethiaq22@gmail.com>
2019-11-03 19:15:58 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, goal) {
+    return drivers.filter(match => 
+        match.toLowerCase() === goal.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letter) {
+    return drivers.filter(match =>
+        match.toLowerCase().indexOf(letter.toLowerCase()) === 0 
+    )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(match =>
+        match.name === name)
+}
